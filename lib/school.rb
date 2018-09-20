50cab921511b320eeaebeae77c60247e0e3db57e
Rajvir Rai <rsr1992@gmail.com>
2018-09-20 00:19:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+
+attr_accessor :name, :roster
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+def add_student(student, grade)
+  roster[grade] ||= []
+  roster[grade] << student
+end
+def grade(student_grade)
+  roster[student_grade]
+end
+
+def sort_students
+  roster.alphabetize
+end
+  
+end
